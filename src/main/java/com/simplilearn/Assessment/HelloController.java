be9db69fe5d3a3955ec20609c6f2e2fc59179b1c
ManasaMattur <77509025+ManasaMattur@users.noreply.github.com>
2021-04-06 19:11:20 +0000

    Update HelloController.java

--- src/main/java/com/simplilearn/Assessment/HelloController.java
+++ src/main/java/com/simplilearn/Assessment/HelloController.java
@@ -1,4 +1,4 @@
-package com.simplilearn.AssessmentProject;
+package com.simplilearn.assessment.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
