880a93983577a54f80ecff1900f2b9c153af763f
ManasaMattur <77509025+ManasaMattur@users.noreply.github.com>
2021-04-06 19:16:43 +0000

    Update ServletInitializer.java

--- src/main/java/com/simplilearn/Assessment/ServletInitializer.java
+++ src/main/java/com/simplilearn/Assessment/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.simplilearn.assessment;
+package com.simplilearn.Assessment;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
